710bbade615546ef1756830672e1f9c32b9cea7c
wusp <15524101372@163.com>
2017-11-17 11:21:16 +0800

    函数传任意数量的参数

---
+++ pizza.py
@@ -0,0 +1,8 @@
+def make_pizza(size,*toppings):
+    """概述要制作的披萨"""
+    print("\nMaking a "+str(size)+"-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("-"+topping)
+    #print(toppings)
+make_pizza(16,'pepperoni')
+make_pizza(12,'mushrooms','greens peppers','extra chese')
